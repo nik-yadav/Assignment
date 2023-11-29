72c9bf09cf580c6e33196cff6f48c65687bcd891
Nikhil Yadav <132257962+nik-yadav@users.noreply.github.com>
2023-11-29 13:58:45 +0530

    Update Assignment3.js

--- Assignment3.js
+++ Assignment3.js
@@ -51,3 +51,23 @@ console.log(inputArr.filter(filler));
 function filler(input){
 	if(input >  5 ) return input;
 }
+
+/*  7  */
+const students = [
+{ name: "Ram", scores: [80, 70, 60] },
+{ name: "Mohan", scores: [80, 70, 90] },
+{ name: "Sai", scores: [60, 70, 80] },
+{ name: "Hemang", scores: [90, 90, 80, 80] },
+];
+students.map((student) => {
+	let total = student.scores.reduce(calculate_average, 0)
+  student['average'] = total/student.scores.length;
+  delete(student.scores)
+  return student;
+})
+
+function calculate_average(total, currentValue){
+	total += currentValue;
+  return total;
+}
+console.log(students);
